adc30014142436d07c09c1a17f9fa3b6248f583a
Test <Test@mail.com>
2020-10-01 20:09:56 +0300

    #0 commit. start repository

---
+++ Default.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Pokedex
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        //protected void Page_PreRender(object sender, EventArgs e)
+        //{
+        //    Response.Write(DateTime.Now.ToString()+ "Page_PreRender");
+        //}
+        //protected void Page_OnInit(object sender, EventArgs e)
+        //{
+        //    Response.Write(DateTime.Now.ToString() + "Page_OnInit");
+        //}
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write(DateTime.Now.ToString() + "Page_Load");
+            Response.Write(HttpContext.Current.ToString());
+
+            Output.Text += "test" + Server.UrlEncode("http://google.com");
+        }
+    }
+}
